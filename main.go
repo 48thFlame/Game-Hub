34aaaac77bdeb128a556594004ccad8ce011c725
avitar64 <avishai@taragin.com>
2022-05-17 17:45:04 +0300

    ping command

--- main.go
+++ main.go
@@ -14,19 +14,21 @@ func main() {
 		log.Fatalf("Could not read TOKEN.txt:\n%v\n", err)
 	}
 
-	discord, err := discordgo.New("Bot " + string(token))
+	bot, err := discordgo.New("Bot " + string(token))
 	if err != nil {
 		log.Fatalf("Error creating Discord session:\n%v\n", err)
 	}
 
-	err = discord.Open()
+	err = bot.Open()
 	if err != nil {
 		log.Fatalf("Error opening Discord session:\n%v\n", err)
 	}
-	log.Printf("%v is now online!\n", discord.State.User)
+	log.Printf("%v is now online!\n", bot.State.User)
 
-	
-	defer discord.Close()
+	bot.AddHandler(pingCommand)
+
+	// Wait for a quit signal to quit
+	defer bot.Close()
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
@@ -34,4 +36,10 @@ func main() {
 	<-stop
 
 	log.Println("Shutting down...")
+}
+
+func pingCommand(bot *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Content == "+ping" {
+		bot.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
 }
