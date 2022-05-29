b834098254ab6f05383dc343a79c498c9019f231
avitar64 <avishai@taragin.com>
2022-05-29 18:51:09 +0300

    added InteractionEdit renamed things

--- discord/utils.go
+++ discord/utils.go
@@ -8,19 +8,24 @@ import (
 
 const (
 	InstaMessage    = dg.InteractionResponseChannelMessageWithSource
-	WillEditMessage = dg.InteractionResponseDeferredChannelMessageWithSource
+	DefferSendMessage = dg.InteractionResponseDeferredChannelMessageWithSource
 )
 
 func Error(err error) {
 	log.Printf("!! Error: %v\n", err)
 }
 
-func InteractionRespond(s *dg.Session, i *dg.Interaction, t dg.InteractionResponseType, r *dg.InteractionResponseData) error {
+func InteractionRespond(s *dg.Session, i *dg.Interaction, t dg.InteractionResponseType, d *dg.InteractionResponseData) error {
 	return s.InteractionRespond(
 		i,
 		&dg.InteractionResponse{
 			Type: t,
-			Data: r,
+			Data: d,
 		},
 	)
+}
+
+func InteractionEdit(s *dg.Session, i *dg.Interaction, newresp *dg.WebhookEdit) error {
+	_, err := s.InteractionResponseEdit(i, newresp)
+	return err
 }
