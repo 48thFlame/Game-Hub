3de5df74e48edaf0de2fc0d699c0e15a680c5987
avitar64 <avishai@taragin.com>
2022-05-30 18:52:49 +0300

    spilt boost.py to 2 files

--- discord/boost.py
+++ discord/boost.py
@@ -1,6 +1,8 @@
 import hikari
 import lightbulb
 
+from commands.nonGames import loadNonGamesCommands
+
 with open("./discord/TOKEN.txt", "r") as f:
     TOKEN = f.readline().strip()
 
@@ -11,57 +13,9 @@ bot = lightbulb.BotApp(
 )
 
 
-@bot.command
-@lightbulb.command("ping", "ping.")
-@lightbulb.implements(lightbulb.commands.SlashCommand)
-async def ping_command(ctx):
-    pass
-
-
-@bot.command
-@lightbulb.option("question", "The question that the magic-8ball should answer.", str, required=True)
-@lightbulb.command("8ball", "Answers a question with the magic-8ball's answer.")
-@lightbulb.implements(lightbulb.commands.SlashCommand)
-async def ball8_command(ctx):
-    pass
-
-
-@bot.command
-@lightbulb.option('dice', 'Number of dice to roll.', int, required=True, min_value=1, max_value=150)
-@lightbulb.option('sides', 'Number of sides each dice should have.', int, required=True, min_value=4, max_value=16)
-@lightbulb.command('dice', 'Rolls dice.')
-@lightbulb.implements(lightbulb.commands.SlashCommand)
-async def dice_command(ctx):
-    pass
-
-
-@bot.command
-@lightbulb.command('coinflip', 'Flips a coin.')
-@lightbulb.implements(lightbulb.commands.SlashCommand)
-async def coinflip_command(ctx):
-    pass
-
-
-@bot.command
-@lightbulb.command('info', 'Sends some useful information.')
-@lightbulb.implements(lightbulb.commands.SlashCommand)
-async def info_command(ctx):
-    pass
-
-
-@bot.command
-@lightbulb.option('poll', 'The poll.', str, required=True)
-@lightbulb.option(
-    'channel',
-    'The channel to send the poll to.',
-    hikari.TextableGuildChannel,
-    required=True,
-    channel_types=[hikari.ChannelType.GUILD_TEXT]
-)
-@lightbulb.command('poll', 'Send a yes/no poll to the given channel.')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def poll_command(ctx):
-    pass
+@bot.listen(hikari.StartingEvent)
+async def load_commands(event):
+    loadNonGamesCommands(bot)
 
 
 @bot.listen(hikari.StartedEvent)
---
+++ discord/commands/nonGames.py
@@ -0,0 +1,53 @@
+import hikari
+import lightbulb
+
+
+def loadNonGamesCommands(bot: lightbulb.BotApp):
+    print(bot)
+
+    @bot.command
+    @lightbulb.command("ping", "ping.")
+    @lightbulb.implements(lightbulb.commands.SlashCommand)
+    async def ping_command(ctx):
+        pass
+
+    @bot.command
+    @lightbulb.option("question", "The question that the magic-8ball should answer.", str, required=True)
+    @lightbulb.command("8ball", "Answers a question with the magic-8ball's answer.")
+    @lightbulb.implements(lightbulb.commands.SlashCommand)
+    async def ball8_command(ctx):
+        pass
+
+    @bot.command
+    @lightbulb.option('dice', 'Number of dice to roll.', int, required=True, min_value=1, max_value=150)
+    @lightbulb.option('sides', 'Number of sides each dice should have.', int, required=True, min_value=4, max_value=16)
+    @lightbulb.command('dice', 'Rolls dice.')
+    @lightbulb.implements(lightbulb.commands.SlashCommand)
+    async def dice_command(ctx):
+        pass
+
+    @bot.command
+    @lightbulb.command('coinflip', 'Flips a coin.')
+    @lightbulb.implements(lightbulb.commands.SlashCommand)
+    async def coinflip_command(ctx):
+        pass
+
+    @bot.command
+    @lightbulb.command('info', 'Sends some useful information.')
+    @lightbulb.implements(lightbulb.commands.SlashCommand)
+    async def info_command(ctx):
+        pass
+
+    @bot.command
+    @lightbulb.option('poll', 'The poll.', str, required=True)
+    @lightbulb.option(
+        'channel',
+        'The channel to send the poll to.',
+        hikari.TextableGuildChannel,
+        required=True,
+        channel_types=[hikari.ChannelType.GUILD_TEXT]
+    )
+    @lightbulb.command('poll', 'Send a yes/no poll to the given channel.')
+    @lightbulb.implements(lightbulb.SlashCommand)
+    async def poll_command(ctx):
+        pass
