d1b754a8c35c271dac41782773eeb65263a2a07d
avitar64 <avishaitaragin@gmail.com>
2022-08-09 16:46:06 +0300

    added connect4 to terminal

--- terminanl/main.go
+++ terminanl/main.go
@@ -2,7 +2,8 @@ package main
 
 func main() {
 	mastermind := newCommand(newAliases("mastermind", "mm", "m"), mastermindCommand)
+	connect4 := newCommand(newAliases("connect4", "c4", "4"), connect4Command)
 
-	t := newTerminal([]*command{mastermind})
+	t := newTerminal([]*command{mastermind, connect4})
 	t.run()
 }
